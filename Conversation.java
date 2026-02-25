76e61f7551da650a68d8f65feac15d844562228f
leallen2029 <leallen@smith.edu>
2026-02-24 21:53:50 -0500

    save

--- Conversation.java
+++ Conversation.java
@@ -1,28 +1,48 @@
 // You should **not** update any call signatures in this file
 // only modify the body of each function
+import java.util.Scanner;
+
 class Conversation implements ConversationRequirements {
 
   // Attributes 
 
-  /**
-   * Constructor 
+  /*
+    Constructor 
    */
   Conversation() {
-    
+  
   }
 
   /**
    * Starts and runs the conversation with the user
    */
   public void chat() {
+    Scanner input = new Scanner(System.in);
+    System.out.print("How many rounds? ");
+    int rounds = input.nextInt();
+    input.nextLine();
 
+    String transcript = "";
+
+    for (int i = 0; i < rounds; i++) {
+      String line1 = input.nextLine();
+      System.out.println();
+      transcript += line1 + "\n";
+
+      String line2 = input.nextLine();
+      System.out.println();
+      transcript += line2 + "\n";
   }
 
   /**
    * Prints transcript of conversation
    */
-  public void printTranscript() {
 
+  System.out.println("\nTRANSCRIPT:");
+  System.out.print(transcript);
+  input.close();
+
+  public void printTranscript() {
   }
 
   /**
